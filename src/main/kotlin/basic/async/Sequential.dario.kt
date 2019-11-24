bc6351c5b951356b1f8ce1945f56501ef747cc6b
Dario Dessaunet <>
2019-11-24 17:45:02 -0300

    Added more coroutines examples

---
+++ src/main/kotlin/basic/async/Sequential.dario.kt
@@ -0,0 +1,25 @@
+package basic.async
+
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.runBlocking
+import kotlin.system.measureTimeMillis
+
+fun main(args: Array<String>) = runBlocking {
+
+    val time = measureTimeMillis {
+        val one = function1()
+        val two = function2()
+        println("Result is ${one + two}")
+    }
+    println("Total time: $time")
+}
+
+suspend fun function1(): Int {
+    delay(1000)
+    return 15
+}
+
+suspend fun function2(): Int {
+    delay(1000)
+    return 27
+}
